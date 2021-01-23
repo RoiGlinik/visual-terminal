1b7cb552381c00e8074d0fbe0d4292fa11ff5358
Natan Yellin <aantny@gmail.com>
2021-01-23 22:05:30 +0200

    get coreui working

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
 module.exports = {
-    "pluginOptions":  {
-        "electronBuilder": {
-          "nodeIntegration": true, // this may or may not be necessary - you can try without it
-           externals:['node-pty'], // this excludes the node-pty from the front end
-           allowRendererProcessReuse : false,
+    runtimeCompiler: true,
+    configureWebpack: {
+        //Necessary to run npm link https://webpack.js.org/configuration/resolve/#resolve-symlinks
+        resolve: {
+            symlinks: false
+        }
+    },
+    transpileDependencies: [
+        '@coreui/utils'
+    ],
+    pluginOptions: {
+        electronBuilder: {
+            nodeIntegration: true, // this may or may not be necessary - you can try without it
+            externals: ['node-pty'], // this excludes the node-pty from the front end
+            allowRendererProcessReuse: false,
         }
     }
 }
